c6f32aa75b92fe260bf4fd9bdc57cb1a9fe4082e
Your Name <your@email.com>
2025-07-25 17:17:45 +0300

    map concept

---
+++ maping/1.js
@@ -0,0 +1,6 @@
+var information =new Map([
+    ['name','sarishma'],
+    ['age',23],
+    ['place','biratnagar'],
+])
+console.log(information)
---
+++ maping/2.js
@@ -0,0 +1,7 @@
+var info=new Map([
+['name','sarishma'],
+])
+info.set('age',20)
+console.log(info)
+info.delete('name')
+console.log(info)
